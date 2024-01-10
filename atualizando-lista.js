774f846a6acb0b49f039b4bf8048048cfba3640b
Flaviana Ferraz <113718720+FlavianaFXT@users.noreply.github.com>
2024-01-10 12:05:59 -0300

    atualizando-lista

---
+++ atualizando-lista.js
@@ -0,0 +1,13 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+//splice = emendar, juntar
+
+// nomes.splice(1,2);
+
+// nomes.push(Rodrigo);
+
+//de forma mais sintetica:
+
+nomes.splice(1,2, 'Rodrigo');
+
+console.log(nomes);
