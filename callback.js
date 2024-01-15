4efff5478c3e62417c291909a5c854d820435bb3
Flaviana Ferraz <113718720+FlavianaFXT@users.noreply.github.com>
2024-01-15 14:45:27 -0300

    callback

---
+++ callback.js
@@ -0,0 +1,23 @@
+const nomes = ['Evaldo', 'Camis', 'Mari'];
+
+//FORMA 1:
+
+// nomes.forEach(function(nome){
+//     console.log(nome);
+// })
+
+//FORMA 2:
+//ARROW FUNCTION
+
+nomes.forEach((nome) => {
+    console.log(nome);
+})
+
+
+//FORMA 3
+
+// function imprimeNome(nome){
+//     console.log(nome);
+// }
+
+// nomes.forEach(imprimeNome);
